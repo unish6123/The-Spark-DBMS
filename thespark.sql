90e6bb31a45d25e58a8708ea05bd4845582e2e88
Anuj-Khadka <anujkhadka008@gmail.com>
2024-04-17 22:43:05 -0400

    Update Database table

---
+++ thespark.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Student 
+(
+    stuID integer,
+    stuName varchar(40)
+    stuPassword text,
+    stuAddress varchar(50)
+    cellNumber integer,
+    constraint student_id_pk primary key (stuID)
+)
+
