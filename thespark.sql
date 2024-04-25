fdd157c45963c5a9b6129e6d4478b424124a9e81
Anuj-Khadka <anujkhadka008@gmail.com>
2024-04-24 23:57:48 -0400

    Update databse

--- thespark.sql
+++ thespark.sql
@@ -1,4 +1,3 @@
-/* Creating Students Table */
 CREATE TABLE Student 
 (
     stuID integer,
@@ -25,10 +24,144 @@ insert into Student
 values (5, 'Emily Davis', 'password123', '789 Maple St, San Francisco, CA', 3456789012);
 
 
-/* Creating Instructor Table */
+CREATE TABLE Instructor
+(
+   instructorID integer,
+   instructorName varchar(40),
+   instructorQual varchar(250),
+   instructorAddress varchar(100),
+   instructorDepartment varchar(100),
+   constraint instructor_instructorID_pk primary key(instructorID)
+);
 
-/* Creating Assessment Table */
+insert into Instructor
+values (1, 'John Mill', 'Masters in Mathematics', '121 Bloomfield Ave, Newark, NJ', 'Mathematics');
+
+insert into Instructor
+values (2, 'Joanna Gaines', 'Masters in Biology', '12 St. James Place, Montclair, NJ', 'Biology');
+
+
+insert into Instructor
+values (3, 'William Sanchez', 'Masters in Physics', '99 Martin Ave, Caldwell, NJ', 'Physics');
+
+
+insert into Instructor
+values (4, 'Brianna Levine', 'Bachelor in Computer Information Systems', '89 1st Street, Roseville, NJ', 'Computer Science');
+
+
+insert into Instructor
+values (5, 'Pricsilla Monroe', 'PHD in History', '120 Bloomfield Ave, Caldwell, NJ', 'History');
+
 
 /* Creating Courses Table */
+CREATE TABLE Courses
+(
+    courseID integer,
+    courseName varchar(100),
+    description varchar(255),
+    difficulty varchar(50),
+    professorName varchar(100),
+    constraint course_id_pk primary key (courseID)
+);
+
+/* Inserting sample data into the Courses table */
+insert into Courses
+values (1, 'Mathematics 101', 'Introduction to basic mathematics', 'Beginner', 'John Mill');
+
+insert into Courses
+values (2, 'Physics 201', 'Intermediate level physics course', 'Intermediate', 'William Sanchez');
+
+insert into Courses
+values (3, 'Computer Science 301', 'Advanced programming concepts', 'Advanced', 'Brianna Levine');
+
+insert into Courses
+values (4, 'Art History 101', 'Introduction to art history', 'Beginner', 'Priscilla Monroe');
+
+insert into Courses
+values (5, 'Psychology 201', 'Intermediate level psychology course', 'Intermediate', 'Joanna Gaines');
+
+    
+CREATE TABLE Assessment (
+ assessmentID integer,
+ courseID integer,
+ title varchar(255),
+ passingScore integer,
+ dueDate Date,
+ assessmentTime varchar(255),
+ constraint assessment_assessmentID_pk primary key(assessmentID),
+ constraint courseID_fk foreign key (courseID) references Courses(courseID)
+);
+
+insert into  Assessment 
+values (1001, 1, 'Quiz' , 60, TO_DATE('2024/05/20', 'yyyy/mm/dd'), '00:60:00');
+
+insert into  Assessment
+values  (1002, 2, 'Mid-Term',60,TO_DATE('2024/05/20', 'yyyy/mm/dd'),'00:60:00');
+
+insert into  Assessment 
+values (1003, 3, 'Quiz Basics' , 60, TO_DATE('2024/05/20', 'yyyy/mm/dd'), '00:60:00');
+
+insert into  Assessment
+values  (1004, 4, 'Mid-Term Basics',60,TO_DATE('2024/05/20', 'yyyy/mm/dd'),'00:60:00');
+
+insert into  Assessment
+values (1005, 5, 'Quiz' , 60, TO_DATE('2024/05/20', 'yyyy/mm/dd'), '00:60:00');
+
 
 /* Creating Payment Table */
+CREATE TABLE Invoice
+(
+   invoiceID integer,
+   stuID integer,
+   inVoiceDate Date,
+   paymentType varchar(100),
+   creditBalance integer,
+   totalBalance integer,
+   constraint invoice_invoiceID_pk primary key(invoiceID),
+   constraint stuID_fk foreign key (stuID) references Student(stuID)
+);
+
+insert into Invoice
+values (1, 2, TO_DATE('2024/02/20', 'yyyy/mm/dd'), 'Debit', 300, 3000);
+
+insert into Invoice
+values (2, 3, TO_DATE('2024/02/20', 'yyyy/mm/dd'), 'Credit', 600, 3500);
+
+
+insert into Invoice
+values (3, 4, TO_DATE('2024/02/20', 'yyyy/mm/dd'), 'Cash', 650, 500);
+
+
+insert into Invoice
+values (4, 1, TO_DATE('2024/02/20', 'yyyy/mm/dd'), 'Debit', 790, 6000);
+
+
+insert into Invoice
+values (5, 2, TO_DATE('2024/02/20', 'yyyy/mm/dd'), 'Cheque', 3000, 5700);
+
+
+
+CREATE Table Classes(
+    stuID integer,
+    instructorID integer,
+    constraint primary key(stuID, instructorID)
+    constraint stu_fk foreign key (stuID) references Student(stuID)
+    constraint inst_fk foreign key (instructorID) references Student(instructorID)
+)
+
+CREATE Table StudentRecord(    
+    stuID integer,
+    assessmentID integer,
+    constraint primary key(stuID, assessmentID)
+    constraint stu_fk foreign key (stuID) references Student(stuID)
+    constraint assment_fk foreign key (assessmentID) references Assessment(assessmentID)
+)
+
+CREATE table Rating(
+    stuID integer,
+    courseID integer,
+    constraint primary key(stuID, courseID)
+    constraint stu_fk foreign key (stuID) references Student(stuID)
+    constraint course_fk foreign key (courseID) references Courses(courseID)
+
+)
